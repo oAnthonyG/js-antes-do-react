070faac545b06516055f21dd40012b2cf4447d16
anthony <anthonytguilherme@hotmail.com>
2023-01-09 16:23:53 -0300

    optional chaining

--- main.js
+++ main.js
@@ -2,32 +2,55 @@
 
 //Object
 
-/*const user = {
-    name: 'anthony',
+const user1 = {
+    name1: 'anthony',
     idade: 24,
     address: {
         street: 'Rua teste',
         number: 123,
     },
-};*/
+};
 
 // Destruturação
 
-//const { address, idade: age, nickname = 'Guilherme' } = user
+const { address, idade: age1, nickname = 'Guilherme' } = user1
 
-/*function mostraIdade({ idade }) {
+function mostraIdade({ idade }) {
     return idade
-}*/
+}
 
 // Rest operator
 
-//const { name, ...rest } = user;
-/*
+/*const { name1, ...rest } = user1;*/
+
+
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const [first, , third, ...rest] = array
-*/
 
 
+// Short Syntax
 
+const name2 = 'Anthony';
+const age2 = 24;
 
-document.body.innerHTML = JSON.stringify({ first, third, rest })
+const user = {
+    name2,
+    age2
+};
+
+// Optional Chaining
+
+const user2 = {
+    name: 'Anthony',
+    age: 24,
+    address: {
+        street: 'Rua teste',
+        number: 176,
+        zip: {
+            code: '12345678',
+            city: 'são paolo'
+        }
+    },
+};
+
+document.body.innerHTML= user2.address?.zip?.code ?? 'não informado'
